5f48212d94cf226e3f94186951aeb46939234f44
Narayan Poudel <poudelnarayan434@gmail.com>
2022-06-24 09:49:49 +0545

    dynamic styling class

--- styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,38 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
+  /*
+    `` -> backticks is called template literal
+    whatever you type in between will be treated as a regular string
+    and also you can inject the dynamic string
+
+  */
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        {/* <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label> */}
+        {/* <input style={{ borderColor: !isValid ? 'red' : 'black', background: !isValid ? 'salmon' : 'transparent' }} type="text" onChange={goalInputChangeHandler} /> */}
+        <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
+
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
