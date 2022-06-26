47223c677ea96f4cb020060223d4b3727e2aa869
Narayan Poudel <poudelnarayan434@gmail.com>
2022-06-26 19:12:58 +0545

    need of useEffect

--- useEffetuseReducerExampleProject/src/App.js
+++ useEffetuseReducerExampleProject/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,9 +7,24 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const savedUserLoginData = localStorage.getItem('isLoggedIn');
+
+  if (savedUserLoginData === '1') {
+    setIsLoggedIn(true);
+  }
+  /*
+  Huge disadvantage of this approcah is that wee would create an infinite loop.
+  Because we check for loggedinData being stored  , If it is stores we set loggedin to true.
+  and whenever we call a state setting function , the component function re-executes and therefore the loop continues.
+  So we need to use a different approach.
+  We need to use useEffect here. that allows us to control when the function runs
+  */
+
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
