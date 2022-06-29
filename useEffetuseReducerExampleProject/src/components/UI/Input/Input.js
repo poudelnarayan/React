02f00f361e118fe34188844a7ded2445f21ebc4e
Narayan Poudel <poudelnarayan434@gmail.com>
2022-06-29 15:30:13 +0545

    using useImperativeHandle hook

--- useEffetuseReducerExampleProject/src/components/UI/Input/Input.js
+++ useEffetuseReducerExampleProject/src/components/UI/Input/Input.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from 'react';
 
 import classes from './Input.module.css';
 
-const Input = props => {
+const Input = React.forwardRef((props, refs) => {
+    const inputRef = useRef();
+
+    const activate = () => {
+        inputRef.current.focus();
+    }
+
+    useImperativeHandle(refs, () => {
+        return {
+            focus: activate
+        }
+    });
+
     return <div
         className={`${classes.control} ${props.isValid === false ? classes.invalid : ''
             }`}
     >
         <label htmlFor={props.id}>{props.label}</label>
         <input
+            ref={inputRef}
             type={props.type}
             id={props.id}
             value={props.value}
@@ -16,6 +29,6 @@ const Input = props => {
             onBlur={props.onBlur} // whenever the input field looses focus , the validation function is called
         />
     </div>
-}
+});
 
 export default Input;
