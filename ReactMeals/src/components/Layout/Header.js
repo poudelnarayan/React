0c5137bb7fbaf79fb7c4b4b5eaf8ec0fd800ec37
Narayan Poudel <poudelnarayan434@gmail.com>
2022-06-30 19:27:01 +0545

    header-section

---
+++ ReactMeals/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return <>
+        <header className={classes.header} >
+            <h1>ReactMeals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt='meal' />
+        </div>
+    </>
+}
+
+export default Header;
