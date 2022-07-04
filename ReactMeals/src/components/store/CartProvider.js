bd6494d55b29e3d3f88d5f0c4f45e36853da5a9d
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-04 21:41:57 +0545

    Adding a cart reducer

--- ReactMeals/src/components/store/CartProvider.js
+++ ReactMeals/src/components/store/CartProvider.js
@@ -1,14 +1,38 @@
+import { useReducer } from 'react';
+
 import React from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+}
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedItem = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            item: updatedItem,
+            amount: updatedTotalAmount,
+        };
+    }
+    return defaultCartState;
+}
 
 const CartProvider = props => {
-    const addCartItemHandler = item => { }
-    const removeCartItemHandler = id => { }
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+
+    const addCartItemHandler = item => {
+        dispatchCartAction({ type: 'ADD', item: item });
+    }
+    const removeCartItemHandler = id => {
+        dispatchCartAction({ type: 'REMOVE', id: id });
+    }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addCartItemHandler,
         removeItem: removeCartItemHandler,
     };
