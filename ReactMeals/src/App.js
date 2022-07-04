eccad58e358535a31b132e99f106b02c5c360b26
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-04 21:11:48 +0545

    using cart-provider and cart-context

--- ReactMeals/src/App.js
+++ ReactMeals/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from './components/Cart/Cart';
+import CartProvider from "./components/store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -14,13 +15,13 @@ function App() {
     setCartIsShown(false);
   }
   return (
-    <>
-      {cartIsShown && <Cart onClose={ hideCartHandler} />}
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
