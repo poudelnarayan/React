971f0a7d15868ea31f13efea2d30056935e84f31
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-08 22:58:08 +0545

    handling error sytle-2

--- SendingHttpRequest/src/App.js
+++ SendingHttpRequest/src/App.js
@@ -39,6 +39,20 @@ function App() {
   }
 
 
+  let content = <p>Found no movies</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
 
 
   return (
@@ -47,10 +61,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && error && <p>{error}</p>}
+        {content}
       </section>
     </React.Fragment>
   );
