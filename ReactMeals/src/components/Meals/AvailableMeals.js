1db85d124f7c4cc397cf50a41ffd04ac3fe3a488
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-16 18:36:09 +0545

    fetching data from firebase and handling loading state

--- ReactMeals/src/components/Meals/AvailableMeals.js
+++ ReactMeals/src/components/Meals/AvailableMeals.js
@@ -1,53 +1,59 @@
-import classes from './AvailableMeals.module.css';
-import Card from '../UI/Card';
+import { useEffect, useState } from "react";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
 
-import MealItem from './mealItem/MealItem';
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
+import MealItem from "./mealItem/MealItem";
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal =>
-        < MealItem id={meal.id} key={meal.id}  name={meal.name} description={meal.description} price={meal.price} />
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-js-300cc-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading.....</p>
+      </section>
     );
+  }
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
-    return (<section className={classes.meals}>
-        <Card>
-            <ul>
-                {mealsList}
-            </ul>
-
-        </Card>
-
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-    );
-
-
+  );
 };
 
 export default AvailableMeals;
