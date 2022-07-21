3b2f32bc419b2c4a7ac9e4e00c6d33898899818e
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-21 19:17:58 +0545

    providing the store

--- ReduxProject/counterExample/src/index.js
+++ ReduxProject/counterExample/src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import Store from "./store/index";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
