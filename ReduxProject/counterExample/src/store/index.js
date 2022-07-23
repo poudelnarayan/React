8d5b19245af318f7d6051c6279c25012630fb63b
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-23 21:43:18 +0545

    how to work with redux state correctly

--- ReduxProject/counterExample/src/store/index.js
+++ ReduxProject/counterExample/src/store/index.js
@@ -4,15 +4,35 @@ const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
+    // case "INCREMENT":
+    //  state.counter++;
+
+    //   return {
+    //     counter: state.counter,
+    //     showCounter: state.showCounter,
+    //   };
+
+    /*
+    This is a super bad example of using reducer . We musn't use reducer to change state.
+    this can leads to bugs, unpredictable behaviour and hard to debug.
+    We should use action to change state and must always return a brand new state.
+    */
+
     case "INCREMENT":
-      return { counter: state.counter + 1, showCounter: state.showCounter };
+      return {
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
     case "INCREASE":
       return {
         counter: state.counter + action.amount,
         showCounter: state.showCounter,
       };
     case "DECREMENT":
-      return { counter: state.counter - 1, showCounter: state.showCounter };
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
     case "TOGGLE_COUNTER":
       return {
         counter: state.counter,
