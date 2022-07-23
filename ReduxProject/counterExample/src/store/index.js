b88c0480d33af3eb68dfd5404de86e00122663a8
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-23 22:08:20 +0545

    connecting redux toolkit state

--- ReduxProject/counterExample/src/store/index.js
+++ ReduxProject/counterExample/src/store/index.js
@@ -1,9 +1,8 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState, // initialState: initialState (same in modern js)
   reducers: {
@@ -30,46 +29,8 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    // case "INCREMENT":
-    //  state.counter++;
-
-    //   return {
-    //     counter: state.counter,
-    //     showCounter: state.showCounter,
-    //   };
-
-    /*
-    This is a super bad example of using reducer . We musn't use reducer to change state.
-    this can leads to bugs, unpredictable behaviour and hard to debug.
-    We should use action to change state and must always return a brand new state.
-    */
-
-    case "INCREMENT":
-      return {
-        counter: state.counter + 1,
-        showCounter: state.showCounter,
-      };
-    case "INCREASE":
-      return {
-        counter: state.counter + action.amount,
-        showCounter: state.showCounter,
-      };
-    case "DECREMENT":
-      return {
-        counter: state.counter - 1,
-        showCounter: state.showCounter,
-      };
-    case "TOGGLE_COUNTER":
-      return {
-        counter: state.counter,
-        showCounter: !state.showCounter,
-      };
-    default:
-      return state;
-  }
-};
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
