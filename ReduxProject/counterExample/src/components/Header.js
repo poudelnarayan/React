b7657488b3a885444c19ed897f3cd85a071b2f6e
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-23 23:22:34 +0545

    splitting our code

--- ReduxProject/counterExample/src/components/Header.js
+++ ReduxProject/counterExample/src/components/Header.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Header.module.css";
 
-import { authActions } from "../store";
+import { authActions } from "../store/auth";
 
 const Header = () => {
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
--- ReduxProject/counterExample/src/store/index.js
+++ ReduxProject/counterExample/src/store/index.js
@@ -1,52 +1,10 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialCounterState = { counter: 0, showCounter: true };
-const initialAuthState = { isAuthenticated: false };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-      /*
-    It seems to be allowed here. But we still must not manupulate the existing state.but the good thing is 
-    when using Redux ToolKit and its function like create slice, we can't accidently manipulate the existing state 
-    because redux toolkit internally uses another package called imgur , which will detect code like this and which
-    will automatically clone the existing state , create a new state object object  , keep all the state which we're not editing 
-    and override the state which we are editing in an immutable way.we still have immutable code here even though it doesnot look
-    like this because of this interally used package and therefore we can't accidently manipulate the existing state.
-    */
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter += action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 export default store;
