c598bee953e3fe23d78cc95c012f8c822102eb16
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-25 14:25:26 +0545

    updating cart quantity

--- ReduxProject/reduxRefresh/src/components/Cart/CartButton.js
+++ ReduxProject/reduxRefresh/src/components/Cart/CartButton.js
@@ -1,16 +1,17 @@
-import { useDispatch } from "react-redux/es/exports";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 import { uiActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const toggleCartHandler = () => {
     dispatch(uiActions.toggleCart());
   };
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
