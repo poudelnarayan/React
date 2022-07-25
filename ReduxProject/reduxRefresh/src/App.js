7488140d96afada384332deb8173166123d7ee94
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-25 15:17:33 +0545

    using useEffect with Redux to handle async code and connect to firebase

--- ReduxProject/reduxRefresh/src/App.js
+++ ReduxProject/reduxRefresh/src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +7,14 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-js-300cc-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT", // this overrides the data , for this case overrides all the data in cart
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
