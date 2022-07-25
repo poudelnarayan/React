49d12c57c685a929c7fc13624985276e32b73c34
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-25 13:55:08 +0545

    using reduc actions in ui

--- ReduxProject/reduxRefresh/src/components/Cart/CartButton.js
+++ ReduxProject/reduxRefresh/src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux/es/exports";
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
