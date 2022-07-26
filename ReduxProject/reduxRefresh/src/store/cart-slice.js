d8698a0aaf3af44c7427ae1cf5ccc3ea2e1f1239
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-26 20:08:10 +0545

    using an action creator Thunk

--- ReduxProject/reduxRefresh/src/store/cart-slice.js
+++ ReduxProject/reduxRefresh/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -38,5 +39,47 @@ const cartSlice = createSlice({
   },
 });
 
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-js-300cc-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT", // this overrides the data , for this case overrides all the data in cart
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 export const cartActions = cartSlice.actions;
 export default cartSlice;
