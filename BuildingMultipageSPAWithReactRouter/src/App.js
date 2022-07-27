6ac057500fef18aba9b043c6dca485d24ccb6b72
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-27 10:18:39 +0545

    working with links

--- BuildingMultipageSPAWithReactRouter/src/App.js
+++ BuildingMultipageSPAWithReactRouter/src/App.js
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
-    <Routes>
-      <Route path="welcome" element={<Welcome />}></Route>
-      <Route path="products" element={<Products />}></Route>
-    </Routes>
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="products" element={<Products />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
