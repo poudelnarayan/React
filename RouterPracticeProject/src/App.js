9d6e29369f65a37498602c7b06c0253b89f92e22
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-28 21:00:36 +0545

    adding a _Not_Found_Page

--- RouterPracticeProject/src/App.js
+++ RouterPracticeProject/src/App.js
@@ -5,17 +5,19 @@ import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import Comments from "./components/comments/Comments";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Navigate to="quotes" />} />
+        <Route path="/" element={<Navigate to="quotes" />} /> 
         <Route path="quotes" element={<AllQuotes />} />
         <Route path="quotes/:quoteId" element={<QuoteDetail />}>
           <Route path="comments" element={<Comments />} />
         </Route>
         <Route path="new-quotes" element={<NewQuote />} />
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </Layout>
   );
