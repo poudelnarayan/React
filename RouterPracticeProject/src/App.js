c32c51da50a03ed6f0f070fac318721112151aac
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-29 23:17:45 +0545

    adding lazy loading

--- RouterPracticeProject/src/App.js
+++ RouterPracticeProject/src/App.js
@@ -1,24 +1,36 @@
+import React, { Suspense } from "react";
+
 import { Route, Routes, Navigate } from "react-router-dom";
 
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
-import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const Comments = React.lazy(() => import("./components/comments/Comments"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="quotes" />} />
-        <Route path="quotes" element={<AllQuotes />} />
-        <Route path="quotes/:quoteId" element={<QuoteDetail />}>
-          <Route path={`comments`} element={<Comments />} />
-        </Route>
-        <Route path="new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="quotes" />} />
+          <Route path="quotes" element={<AllQuotes />} />
+          <Route path="quotes/:quoteId" element={<QuoteDetail />}>
+            <Route path={`comments`} element={<Comments />} />
+          </Route>
+          <Route path="new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
