7173b1a4459d6281c05311e0682865dc674d2f9e
Narayan Poudel <poudelnarayan434@gmail.com>
2022-08-01 22:01:50 +0545

    adding logouts

--- AddingAuthentication/src/components/Layout/MainNavigation.js
+++ AddingAuthentication/src/components/Layout/MainNavigation.js
@@ -9,13 +9,17 @@ const MainNavigation = () => {
 
   const isLoggedIn = authCtx.isLoggedIn;
 
+  const logoutHandler = () => {
+    authCtx.logout();
+    // optional: redirect the user
+  };
+
   return (
     <header className={classes.header}>
       <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
 
-      
       <nav>
         <ul>
           {!isLoggedIn && (
@@ -30,7 +34,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
