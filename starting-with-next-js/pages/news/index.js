a00332611a73029f5fac14aba8eeea83d38e7d8b
Narayan Poudel <poudelnarayan434@gmail.com>
2022-08-04 00:13:00 +0545

    linking between pages

--- starting-with-next-js/pages/news/index.js
+++ starting-with-next-js/pages/news/index.js
@@ -1,7 +1,22 @@
 // our-domain.com/news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js">NextJS</Link>
+        </li>
+        <li>
+          <Link href="news/react-js">ReactJS</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
