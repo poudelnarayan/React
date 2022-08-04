1589b9a2f959cc5dba32860dc75d836ea37ff4c7
Narayan Poudel <poudelnarayan434@gmail.com>
2022-08-04 07:46:22 +0545

    adding custom components and CSS modules

---
+++ starting-with-next-js/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.traveller.com.au/content/dam/images/g/o/3/6/k/o/image.related.articleLeadwide.620x349.go36hx.png/1460702656253.jpg"
+      title="First Meetup"
+      address="Kathmandu Nepal"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
