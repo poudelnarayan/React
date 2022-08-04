db1fcc98291201cb4f8dec47ff031db830fc5f8e
Narayan Poudel <poudelnarayan434@gmail.com>
2022-08-04 06:56:29 +0545

    outputting the list of meetups

---
+++ starting-with-next-js/pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from "../components/meetups/MeetupList";
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first Meetup",
+    image:
+      "https://www.traveller.com.au/content/dam/images/g/o/3/6/k/o/image.related.articleLeadwide.620x349.go36hx.png/1460702656253.jpg",
+    address: "Nepal,Pokhara",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://www.traveller.com.au/content/dam/images/g/o/3/6/k/o/image.related.articleLeadwide.620x349.go36hx.png/1460702656253.jpg",
+    address: "Nepal,Pokhara",
+    description: "This is a Second meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
