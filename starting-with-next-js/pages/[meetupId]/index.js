c589d39e491a9524db26af71eb1386f2c86e6e34
Narayan Poudel <poudelnarayan434@gmail.com>
2022-08-06 23:48:16 +0545

    working with params for SSG data fetching and preparing paths with getStaticPaths and working with fallback pages

--- starting-with-next-js/pages/[meetupId]/index.js
+++ starting-with-next-js/pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId; // meetupId because its the dynamic name in the folder
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://www.traveller.com.au/content/dam/images/g/o/3/6/k/o/image.related.articleLeadwide.620x349.go36hx.png/1460702656253.jpg",
+        title: "First Meetup",
+        address: "Kathmandu Nepal",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
