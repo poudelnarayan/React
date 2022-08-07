af110d3d4a72cd9bfb960292eb9e1f75d859ccda
Narayan Poudel <poudelnarayan434@gmail.com>
2022-08-07 23:09:22 +0545

    working with MongoDB

--- starting-with-next-js/pages/api/new-meetup.js
+++ starting-with-next-js/pages/api/new-meetup.js
@@ -1,10 +1,27 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (res.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:admin@cluster0.gcwqhk1.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne({ data });
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
 
